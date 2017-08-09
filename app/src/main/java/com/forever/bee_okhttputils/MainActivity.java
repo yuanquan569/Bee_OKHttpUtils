e1f4050685130371f7cedeeeef6b8a552d02b552
Yancy <yuanquan569@163.com>
2017-08-09 22:06:27 +0800

    add demo

--- app/src/main/java/com/forever/bee_okhttputils/MainActivity.java
+++ app/src/main/java/com/forever/bee_okhttputils/MainActivity.java
@@ -2,6 +2,10 @@ package com.forever.bee_okhttputils;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.forever.okhttputils.CallBack;
+import com.forever.okhttputils.OKHttpManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OKHttpManager.getInstance_().get("http://www.csdn.net/", new CallBack() {
+            @Override
+            public void onError(Exception e) {
+
+            }
+
+            @Override
+            public void onResponse(final Object response) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ((TextView)findViewById(R.id.tv_ok)).setText(response.toString());
+                    }
+                });
+
+
+            }
+        });
+
+
     }
 }
